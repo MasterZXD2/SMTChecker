d826f5dc7487e88e49207b2b01d1cb63f6781fd0
MasterZXD2 <goten8615xd@gmail.com>
2025-06-14 09:48:15 +0700

    save user_action.php before merge

--- user_action.php
+++ user_action.php
@@ -8,13 +8,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $place = $_POST['place'] ?? 'ไม่ทราบสถานที่';
     $coords = $_POST['location'] ?? 'ไม่ทราบพิกัด';
 
-    $location = $place . " (" . $coords . ")";
+    //$location = $place . " (" . $coords . ")";
+
+    $googleMapLink = $coords
+        ? "https://www.google.com/maps?q=" . urlencode($coords)
+        : '';
 
     // ตัวอย่าง: บันทึกลง log หรือเก็บใน database
     //file_put_contents("checkin_log.txt", "ชื่อ: $name ม.$m เกิด: $date สถานที่: $location\n", FILE_APPEND);
 
     echo "เช็คอินเรียบร้อย!<br>";
-    echo "ชื่อ: $name<br>ม.: $m<br>เกิด: $date<br>ตำแหน่ง: $location";
+    echo "ชื่อ: $name<br>ม.: $m<br>เกิด: $date<br>ตำแหน่ง: $place";
+
+     if ($googleMapLink) {
+        echo "ดูแผนที่: <a href='$googleMapLink' target='_blank'>$coords</a>";
+    } else {
+        echo "ไม่พบพิกัด";
+    }
 } else {
     echo "Method ไม่ถูกต้อง";
 }
