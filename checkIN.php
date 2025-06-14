2282d347ee6df30d665a0fa7d234c40d04703434
MasterZXD2 <goten8615xd@gmail.com>
2025-06-14 11:37:49 +0700

    add checkIN.php

---
+++ checkIN.php
@@ -0,0 +1,36 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$_SESSION['id'] = $_SESSION['user'][0];
+$_SESSION['name'] = $_SESSION['user'][2];
+$_SESSION['m'] = $_SESSION['user'][3];
+$_SESSION['date'] = $_SESSION['user'][1];
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>SMTC User</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <img alt="check" src="images/clipart1147334.png" />
+</body>
+
+</html>
