36a02a5fc729e63cf74aa50ae8d650935f6ce56b
MasterZXD2 <goten8615xd@gmail.com>
2025-06-14 09:37:00 +0700

    save user before merge

--- user_action.php
+++ user_action.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_SESSION['name'];
     $m = $_SESSION['m'];
     $date = $_SESSION['date'];
-    $location =  $_POST['placeField'] + "(" + $_POST['location'] + ")" ?? 'ไม่พบตำแหน่ง';
+    $location =  $_POST['place'] + "(" + $_POST['location'] + ")" ?? 'ไม่พบตำแหน่ง';
 
     // ตัวอย่าง: บันทึกลง log หรือเก็บใน database
     //file_put_contents("checkin_log.txt", "ชื่อ: $name ม.$m เกิด: $date สถานที่: $location\n", FILE_APPEND);
