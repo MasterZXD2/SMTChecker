7e0e690141f3af1914eca5c48f29e36f18c8d6fa
MasterZXD2 <goten8615xd@gmail.com>
2025-06-25 22:38:56 +0700

    save index.php before merge

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 session_start();
 
+$userAgent = strtolower($_SERVER['HTTP_USER_AGENT']);
+
+if (strpos($userAgent, "line") === false) {
+    echo "กรุณาเปิดจากเว็บจากลิ้งที่อาจารส่งใน LINE เท่านั้น";
+    exit;
+}
+
 if(!isset($_SESSION["user"])){
     header("location: login.php");
 } else {
